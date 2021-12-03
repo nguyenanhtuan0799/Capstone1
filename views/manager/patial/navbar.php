8ba12ce6705a68c9587d10a27b5b975d64235a11
nguyenanhtuan0799 <nguyenanhtuan0799@gmail.com>
2021-12-03 14:09:22 +0700

    export

--- views/manager/patial/navbar.php
+++ views/manager/patial/navbar.php
@@ -17,7 +17,7 @@
             <i class="bx bx-plus-medical js-navbar-add active"></i>
             <i class="bx bx-minus js-navbar-remove"></i>
           </div>
-          <a href="" class="navbar-item__link">
+          <a href="../../views/manager/managerExport.php" class="navbar-item__link">
             Export the Timesheet
           </a>
         </li>
